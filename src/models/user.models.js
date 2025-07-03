806106bc75aa6c3da9051ed81c510a936698fe01
Vinay Nain <vinaynain2601@gmail.com>
2025-07-03 10:49:23 +0530

    fixed the incorrect password as await wasn't defined in the prehook

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,6 +1,6 @@
 import mongoose, { Schema, Types } from "mongoose";
 import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 const userSchema = new Schema(
   {
     // add the fields here
@@ -59,19 +59,17 @@ userSchema.pre("save", async function (next) {
   // this has access to all the info of the userSchema
   // this.passowrd means user's given password
 
-  if (!this.isModified("password")) return next();  // this means if the password field is not being modified then just simply return and go to next middleware or end poitn  and for the first time we are saving it for the first time not modifying 
-  this.password = bcrypt.hash(this.password, 10);
+  if (!this.isModified("password")) return next(); // this means if the password field is not being modified then just simply return and go to next middleware or end poitn  and for the first time we are saving it for the first time not modifying
+  this.password = await bcrypt.hash(this.password, 10);
 
   next();
 });
 
-//Now we compare the password -> js prototype -> schema.methods.method 
+//Now we compare the password -> js prototype -> schema.methods.method
 
-userSchema.methods.isPasswordCorrect = async function(password)
-{
-  return await bcrypt.compare(password, this.password); //bool val return 
-}
-
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password); //bool val return
+};
 
 /*now what is the proof that user is logged in? or signedIn?
 1. for that we need to generate some access token or refreshToken
@@ -80,47 +78,42 @@ userSchema.methods.isPasswordCorrect = async function(password)
 .. basically Access token and refresh token both are the auth token -> the access token we give user for a short period of time but the refresh token we keep it in the database for long time 
 */
 
-
 //=======GENERATE TOKEN ============
 
-userSchema.methods.generateAccessToken = function(){
-  //short life access token 
-  // string based on the info we have provided with the secret key and the time we have set too 
+userSchema.methods.generateAccessToken = function () {
+  //short life access token
+  // string based on the info we have provided with the secret key and the time we have set too
   return jwt.sign(
     {
-      _id: this._id, 
-      email: this.email, 
-      username: this.username, 
-      fullname: this.fullname
-    }, 
-    //here comes the secret key 
-    process.env.ACCESS_TOKEN_SECRET, 
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullname: this.fullname,
+    },
+    //here comes the secret key
+    process.env.ACCESS_TOKEN_SECRET,
     {
-      expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
     }
-  )
+  );
+};
 
-}
-
-/// we can force logout too on the basis of the refreshToken 
-userSchema.methods.generateRefreshToken = function(){
-  //long life access token 
-  // string based on the info we have provided with the secret key and the time we have set too 
+/// we can force logout too on the basis of the refreshToken
+userSchema.methods.generateRefreshToken = function () {
+  //long life access token
+  // string based on the info we have provided with the secret key and the time we have set too
   return jwt.sign(
     {
-      _id: this._id, 
-    // we dont need all that info as we just want to save a single token to a user model and we just need id for that 
-    }, 
-    //here comes the secret key 
-    process.env.REFRESH_TOKEN_SECRET, 
+      _id: this._id,
+      // we dont need all that info as we just want to save a single token to a user model and we just need id for that
+    },
+    //here comes the secret key
+    process.env.REFRESH_TOKEN_SECRET,
     {
-      expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
     }
-  )
-
-}
-
-
+  );
+};
 
 // if the User doc doesnt exist mongoose will create a Model called User based on userSchema
 export const User = mongoose.model("User", userSchema); // now this User is not just an ordinary variable it has all the functionality to do stuff like find add all the crud operation and all the things will reflect in the database model of users (mongoDb define later in the collection as "lowercase plural" so User become users )
